772996195ed372c63a5dd832960f5aa314e9c351
xcbcaron <1348445989@qq.com>
2020-07-16 11:33:02 +0800

    Initial commit

---
+++ gmall0213-logger/src/main/java/com/caron/gmall/logger/controller/LoggerController.java
@@ -0,0 +1,38 @@
+package com.caron.gmall.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Caron
+ * @create 2020-07-15-21:29
+ * @Description
+ * @Version
+ */
+
+@RestController
+public class LoggerController {
+
+    @Autowired
+    KafkaTemplate kafkaTemplate ;
+
+    @RequestMapping("/applog")
+    public String applog(@RequestBody String json){
+        System.out.println(json);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+
+        if(jsonObject.getString("start") != null && jsonObject.getString("start").length()>0){
+            kafkaTemplate.send("GMALL_START0213",json);
+        }else{
+            kafkaTemplate.send("GMALL_START0213",json);
+        }
+
+        return "success";
+    }
+}
---
+++ gmall0213-logger/src/test/java/com/caron/gmall/logger/Gmall0213LoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.caron.gmall.logger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gmall0213LoggerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
